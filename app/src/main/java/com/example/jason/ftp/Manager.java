4a847044e496be3724899f33a095bdf975822acd
Jacob Young <jwyoung07@gmail.com>
2017-12-02 17:57:45 -0800

    Cleaned up code

--- app/src/main/java/com/example/jason/ftp/Manager.java
+++ app/src/main/java/com/example/jason/ftp/Manager.java
@@ -1,36 +1,19 @@
 package com.example.jason.ftp;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.regex.Pattern;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.text.util.Linkify;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
 import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
-import android.widget.AdapterView.OnItemSelectedListener;
 
-public class Manager extends Activity {
+
+public class Manager extends Activity
+{
     private static int ROW_COUNT = -1;
 	private static int COL_COUNT = -1;
 	private Context context;
@@ -46,9 +29,11 @@ public class Manager extends Activity {
 	private TableLayout mainTable;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+	{
         super.onCreate(savedInstanceState);
-		startService(new Intent(this, MusicService.class));
+        Intent startMusic = new Intent(this, MusicService.class);
+		startService(startMusic);
 
 		setContentView(R.layout.main);
 
@@ -58,27 +43,18 @@ public class Manager extends Activity {
 		public void onClick(View v) {
 			Intent i = new Intent(Manager.this, gameActivity.class);
 			startActivity(i);
-			
 		}
-
-		
 	});
-
 		((Button)findViewById(R.id.Menu)).setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				Intent i = new Intent(Manager.this, HSActivity.class);
 				startActivity(i);
-
-
 			}
 
 
 		});
-      
-
-
     }
 
     @Override
@@ -86,6 +62,4 @@ public class Manager extends Activity {
 	{
 		super.onPause();
 	}
-
-
 }

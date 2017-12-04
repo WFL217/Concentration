08e450678b84dbac4c7f77f1f3b8ecfbbc18d929
Andrew <excisionhd1@gmail.com>
2017-12-03 19:37:28 -0800

    fixed up high score landscape and added animation

--- app/src/main/java/com/example/jason/ftp/Manager.java
+++ app/src/main/java/com/example/jason/ftp/Manager.java
@@ -10,17 +10,21 @@ import java.util.regex.Pattern;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.constraint.ConstraintLayout;
 import android.text.util.Linkify;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -44,6 +48,13 @@ public class Manager extends Activity {
 	
 	int turns;
 	private TableLayout mainTable;
+	TableLayout myLayout;
+	AnimationDrawable animationDrawable;
+	Animation frombottom;
+	Animation fromtop;
+	Button playButton;
+	Button hsButton;
+	TextView mainTitle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,6 +62,24 @@ public class Manager extends Activity {
 //		startService(new Intent(Manager.this, MusicService.class));
 
 		setContentView(R.layout.main);
+
+		myLayout = (TableLayout)findViewById(R.id.bg);
+
+		animationDrawable = (AnimationDrawable)myLayout.getBackground();
+		animationDrawable.setEnterFadeDuration(4000);
+		animationDrawable.setExitFadeDuration(4000);
+		animationDrawable.start();
+
+		playButton = (Button)findViewById(R.id.Play);
+		hsButton = (Button)findViewById(R.id.Menu);
+		mainTitle = (TextView) findViewById(R.id.mainTitle);
+
+		frombottom = AnimationUtils.loadAnimation(this,R.anim.frombottom);
+		fromtop = AnimationUtils.loadAnimation(this,R.anim.fromtop);
+
+		playButton.setAnimation(frombottom);
+		hsButton.setAnimation(frombottom);
+		mainTitle.setAnimation(fromtop);
 
        ((Button)findViewById(R.id.Play)).setOnClickListener(new OnClickListener() {
 		

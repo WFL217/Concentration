5eb30a81c8d26fe8c3d5a88590350b53d483f26a
Andrew <excisionhd1@gmail.com>
2017-12-03 19:37:28 -0800

    fixed up high score landscape and added animation

--- app/src/main/java/com/example/jason/ftp/HSActivity.java
+++ app/src/main/java/com/example/jason/ftp/HSActivity.java
@@ -1,20 +1,32 @@
 package com.example.jason.ftp;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
 
 public class HSActivity extends AppCompatActivity {
+
+    CoordinatorLayout myLayout;
+    AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hs);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        myLayout = (CoordinatorLayout)findViewById(R.id.bg2);
+
+        animationDrawable = (AnimationDrawable)myLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(4000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+
 
         ((Button)findViewById(R.id.menuButton)).setOnClickListener(new View.OnClickListener() {
 
